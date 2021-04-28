2b83b8f18bc1f6832ae135b68272390c76e88c6b
shotius <sh.archemashvili@gmail.com>
2021-04-28 13:08:05 +0400

    header intro

---
+++ src/modules/components/Shared/React App_files/main.96714b0d68709d392bb1.hot-update.js
@@ -0,0 +1,26 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/modules/layout/AppLayout/Header/index.css":
+/*!**********************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-4-1!./node_modules/postcss-loader/src??postcss!./src/modules/layout/AppLayout/Header/index.css ***!
+  \**********************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);
+// Imports
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(true);
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, ".header {\n    height: 80px;\n    background: 'black';\n}\n\n\n.logo {\n    height: 70px;\n    position: absolute;\n}\n\n.logout {\n    margin: 10px;\n    color: black;\n}\n \n.col {\n    display: flex;\n    align-items: center;\n    /* justify-content: center; */\n    /* margin: ; */\n}\n\n.row {\n    background-color: black;\n}\n\n.icon {\n    font-size: 30px;\n    color: white !important\n}\n\n.search {\n    margin-left: 10px !important;\n    background-color: green;\n}\n\n@media screen and (max-width: 576px) {\n    .logo {\n      display: none;\n    }\n  }", "",{"version":3,"sources":["webpack://src/modules/layout/AppLayout/Header/index.css"],"names":[],"mappings":"AAAA;IACI,YAAY;IACZ,mBAAmB;AACvB;;;AAGA;IACI,YAAY;IACZ,kBAAkB;AACtB;;AAEA;IACI,YAAY;IACZ,YAAY;AAChB;;AAEA;IACI,aAAa;IACb,mBAAmB;IACnB,6BAA6B;IAC7B,cAAc;AAClB;;AAEA;IACI,uBAAuB;AAC3B;;AAEA;IACI,eAAe;IACf;AACJ;;AAEA;IACI,4BAA4B;IAC5B,uBAAuB;AAC3B;;AAEA;IACI;MACE,aAAa;IACf;EACF","sourcesContent":[".header {\n    height: 80px;\n    background: 'black';\n}\n\n\n.logo {\n    height: 70px;\n    position: absolute;\n}\n\n.logout {\n    margin: 10px;\n    color: black;\n}\n \n.col {\n    display: flex;\n    align-items: center;\n    /* justify-content: center; */\n    /* margin: ; */\n}\n\n.row {\n    background-color: black;\n}\n\n.icon {\n    font-size: 30px;\n    color: white !important\n}\n\n.search {\n    margin-left: 10px !important;\n    background-color: green;\n}\n\n@media screen and (max-width: 576px) {\n    .logo {\n      display: none;\n    }\n  }"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ __webpack_exports__["default"] = (___CSS_LOADER_EXPORT___);
+
+
+/***/ })
+
+})
+//# sourceMappingURL=main.96714b0d68709d392bb1.hot-update.js.map
