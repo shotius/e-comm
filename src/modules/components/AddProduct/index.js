53e4cf2ab4cc641c48969ba9c0cb08c0a80b05fb
Vaniko Akopashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2021-05-01 20:09:28 +0400

    changed folder structure

--- src/modules/components/AddProduct/index.js
+++ src/modules/components/AddProduct/index.js
@@ -12,6 +12,7 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import dompurify from "dompurify";
 import {fetchProducts} from "../../../redux/actions/productsAction";
+import ProductAddingForm from "./ProductAddingForm";
 
 const sanitizer = dompurify.sanitize;
 const {TextArea} = Input;
@@ -80,108 +81,13 @@ const AddProduct = () => {
     width={"auto"}
     style={{maxWidth: "800px"}}
   >
-    <Form
-      className="add-product-form"
-      form={form}
-      name="login"
-    initialValues={{
+  <ProductAddingForm
+    form={form}
+    selectedImg={selectedImg}
+    beforeImgUpload={beforeImgUpload}
+    handleImgChange={handleImgChange}
 
-      description: "",
-      picture: selectedImg
-    }}>
-      <Form.Item
-        label="Title"
-        name="title"
-        rules={[
-          {
-            required: true,
-            message: 'Please input product title!',
-          },
-        ]}
-      >
-        <Input/>
-      </Form.Item>
-
-      <Form.Item
-        label="Brand"
-        name="brand"
-        rules={[
-          {
-            required: true,
-            message: 'Please input product brand!',
-          },
-        ]}
-      >
-        <Input/>
-      </Form.Item>
-
-      <Form.Item
-        label="Category"
-        name="category"
-        rules={[
-          {
-            required: true,
-            message: 'Please input product category!',
-          },
-        ]}
-      >
-        <Select style={{width: "100%"}}>
-          <Option value="laptops">Laptop</Option>
-          <Option value="smartphones">Smartphone</Option>
-          <Option value="home-appliances">Home Appliances</Option>
-          <Option value="kitchen-appliances">Kitchen Appliance</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item
-        label="Price"
-        name="price"
-
-        rules={[
-          {
-            required: true,
-            message: 'Please input product price!',
-          },
-        ]}
-      >
-        <InputNumber min={0} style={{width: "100%"}}/>
-      </Form.Item>
-
-      <Form.Item
-        label="Description"
-        name="description"
-        rules={[
-          {
-            required: true,
-            message: 'Please input product description!',
-          },
-        ]}
-      >
-        <ReactQuill theme="snow"/>
-      </Form.Item>
-
-      <Form.Item
-        label="Picture"
-        rules={[
-          {
-            required: true,
-            message: 'Please upload product picture!',
-          },
-        ]}
-      >
-        <Upload
-          name="picture"
-          listType="picture"
-          showUploadList={true}
-          beforeUpload={beforeImgUpload}
-          onChange={handleImgChange}
-          fileList={[...selectedImg]}
-        >
-          <Button icon={<CloudUploadOutlined/>}>Upload Image</Button>
-        </Upload>
-      </Form.Item>
-
-    </Form>
+  />
   </Modal>
 }
 
