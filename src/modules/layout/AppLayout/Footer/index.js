a417fefa205f79247073ba181706fb2f7e6d3a96
shotius <sh.archemashvili@gmail.com>
2021-05-04 08:44:41 +0400

    footer is done

---
+++ src/modules/layout/AppLayout/Footer/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Button, Col, Divider, Row, Space } from 'antd'
+import { FacebookOutlined, GithubOutlined, TwitterOutlined, MailOutlined } from '@ant-design/icons'
+import './index.css'
+
+
+const Footer = () => {
+    return (
+        <>
+                <Row className="footer-questions" align="center" justify='center'>
+                    <Space size='middle'>
+                        <Col>
+                        Have some questions about out work?
+                        </Col>
+                        <Col style={{display: "flex"}}>
+                        <Button className="btn-question" >Click Here</Button>
+                        </Col>
+                    </Space>
+                </Row>
+                <Row className="footer-info" justify='space-around'>
+                    <Col span={6}>
+                        <div className="footer-heading">COMPANY</div>
+                        <div className="footer-info">Sweeft Digital</div>
+                    </Col>
+                    <Col span={6}>
+                        <div className="footer-heading">CONTACT</div>
+                        <div className="footer-info">
+                            <div>
+                            Vaniko
+                            <br />
+                                <Space size="middle">
+                                    <a href="https://www.facebook.com/3bbwXScxJrDiKMZtvjVekA"><FacebookOutlined /></a>
+                                    <a href="https://github.com/VanoAkofashvili"><GithubOutlined /></a>
+                                    <a href="#"><MailOutlined /></a>
+                                </Space>
+                            </div>
+                            <hr className="line"/>
+                            <div>
+                            Shota <small>(contributor)</small>
+                            <br />
+                                <Space size="middle">
+                                    <a href="https://www.facebook.com/profile.php?id=100004930128285"><FacebookOutlined /></a>
+                                    <a href="https://github.com/shotius"><GithubOutlined /></a>
+                                    <a href="mailto:sh.archemashvili@gmail.com"><MailOutlined /></a>
+                                </Space>
+                            </div>
+                        </div>
+                       
+                        {/* <div className="footer-info">Shota</div> */}
+                    </Col>
+                    <Col span={6}>
+                        <div className="footer-heading">ADRESS</div>
+                        <div className="footer-info">
+                            <a href="https://www.google.com/maps/dir/41.7044819,44.8703534/41.7087161,44.7722134/@41.6965017,44.7519414,12z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0">
+                                28 Ilia Chavchavadze Avenue, T'bilisi
+                            </a>
+                        </div>
+                    </Col>
+                    <Col span={24}>
+                    <small>&#169;Copyright. Shota and Vaniko are contributors, 2021. All Rights reserved.</small>
+                    </Col>
+                </Row>
+        </>
+    )
+}
+export default Footer
--- src/modules/layout/AppLayout/index.js
+++ src/modules/layout/AppLayout/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Layout } from "antd";
 import Header from "./Header";
 import AddProduct from "../../components/AddProduct";
+import Footer from './Footer'
 
-const { Content, Footer} = Layout;
+const { Content } = Layout;
 
 const AppLayout = ({ children }) => {
   return (
@@ -11,7 +12,7 @@ const AppLayout = ({ children }) => {
       <Header />
       <Content className="content-holder">{children}</Content>
       <AddProduct />
-      <Footer >Footer</Footer>
+      <Footer />
     </Layout>
   );
 };
