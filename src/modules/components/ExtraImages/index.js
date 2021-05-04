7e8c94da37ca154f3aeedb6f4d6e7f936ca611d3
Vaniko Akopashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2021-05-04 17:28:15 +0400

    fixed issues, when no images is provided slider is invisible and so on

--- src/modules/components/ExtraImages/index.js
+++ src/modules/components/ExtraImages/index.js
@@ -3,34 +3,12 @@ import {
   LeftCircleOutlined,
   RightCircleOutlined
 } from "@ant-design/icons"
+import {Image} from "antd";
 import "./index.css"
-
-const sliderData = [
-  {
-    img: "https://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg",
-    id: 1
-  },
-  {
-    img: "https://alfaplusgo.com/assets/images/slides/1601007464.jpg",
-    id: 2
-  },
-  {
-    img: "http://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg",
-    id: 4
-  },
-  {
-    img: "http://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg",
-    id: 5
-  },
-  {
-    img: "http://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg",
-    id: 6
-  }
-]
 
 const onSlider = 3;
 
-export default function ExtraImages() {
+export default function ExtraImages({sliderData=[]}) {
   const sliderRef = useRef(null);
   const [curSlide, setCurSlide] = useState(0);
   const [maxSlide, setMaxSlide] = useState(sliderData.length);
@@ -47,13 +25,9 @@ export default function ExtraImages() {
 
   }, [curSlide])
 
-  const goToSlide = ()  => {
-    slides.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
-    );
-  };
-
   const nextSlide = () => {
+    if (maxSlide <= 3)
+      return
     if (curSlide === maxSlide - onSlider)
       setCurSlide(0)
     else
@@ -61,6 +35,8 @@ export default function ExtraImages() {
   }
 
   const prevSlide = () => {
+    if (maxSlide <= 3)
+      return
     if (curSlide === 0) {
       setCurSlide(maxSlide - onSlider);
     } else {
@@ -69,19 +45,24 @@ export default function ExtraImages() {
   }
 
   return <div className="slider" ref={sliderRef}>
+    <Image.PreviewGroup>
     {sliderData.map((slide, i) => {
       return (
         <div className="slide" style={{transform: `translate(${100 * i}%)`, width: `${100 / onSlider}%`}} key={slide.id}>
-          <img src={slide.img} alt=""/>
+          {/*<img src={slide.img} alt=""/>*/}
+          <Image height={80} src={slide.img} />
         </div>
       )
     })}
+      </Image.PreviewGroup>
+    {sliderData.length ? <>
     <button className="slider__btn slider__btn--left" onClick={prevSlide}>
       <LeftCircleOutlined/>
     </button>
     <button className="slider__btn slider__btn--right" onClick={nextSlide}>
       <RightCircleOutlined/>
     </button>
+    </> : null}
   </div>
 
 }
