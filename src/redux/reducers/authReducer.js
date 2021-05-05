9550c17626a02fa4d87fb0ea46be8835b11e4368
VanoAkofashvili <vanikoakofa@gmail.com>
2021-05-05 22:51:00 +0400

    now application can handle situation when token in localStorage contains Bearer

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -7,32 +7,41 @@ import {
   LOGIN_USER_SUCCESS,
   LOGIN_USER_FAIL,
   LOG_OUT,
-  LOGIN_ERROR_CLEAR
+  LOGIN_ERROR_CLEAR,
 } from "../constants";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+
+let lsToken = localStorage.token.split(" ");
+if (lsToken.length === 2) {
+  lsToken = lsToken[1];
+} else {
+  lsToken = lsToken[0];
+}
+
+console.log(lsToken);
+localStorage.setItem("token", lsToken);
 
 const initState = {
   isLoggedIn: !!localStorage.token,
   loginError: false,
-  user: localStorage.token ? jwt.decode(localStorage.token) : null,
+  user: localStorage.token ? jwt.decode(lsToken) : null,
   token: localStorage.token,
   expirationDate: localStorage.expirationDate,
   userRegisterLoading: false,
   userRegisterError: null,
   userLoginError: false,
-  userLoginLoading: false
-
-}
+  userLoginLoading: false,
+};
 
 export default function authReducer(state = initState, action) {
   switch (action.type) {
-     /******************** LOGIN *****************/
+    /******************** LOGIN *****************/
     case LOGIN_USER_START:
       return {
         ...state,
         userLoginError: null,
-        userLoginLoading: true
-      }
+        userLoginLoading: true,
+      };
     case LOGIN_USER_SUCCESS:
       return {
         ...state,
@@ -41,54 +50,53 @@ export default function authReducer(state = initState, action) {
         token: action.token,
         expirationDate: action.expirationDate,
         isLoggedIn: true,
-        user: action.user
-      }
+        user: action.user,
+      };
     case LOGIN_USER_FAIL:
       return {
         ...state,
         userLoginError: action.error,
         userLoginLoading: false,
         isLoggedIn: false,
-        token: ""
-      }
+        token: "",
+      };
     case LOG_OUT:
       return {
         ...state,
         isLoggedIn: false,
         token: "",
-        user: null
-      }
+        user: null,
+      };
     case LOGIN_ERROR_CLEAR:
       return {
         ...state,
-        userLoginError: null
-      }
+        userLoginError: null,
+      };
     /******************** REGISTER *****************/
     case REGISTER_USER_START:
       return {
-        ...state, 
+        ...state,
         userRegisterLoading: true,
-        userRegisterError: null
-      }
-    case REGISTER_USER_SUCCESS: 
+        userRegisterError: null,
+      };
+    case REGISTER_USER_SUCCESS:
       return {
-        ...state, 
+        ...state,
         userRegisterLoading: false,
-        userRegisterError: null
-      }
+        userRegisterError: null,
+      };
     case REGISTER_USER_FAIL:
       return {
         ...state,
         userRegisterError: action.error,
-        userRegisterLoading: false
-      }
+        userRegisterLoading: false,
+      };
     case REGISTER_ERROR_CLEAR:
       return {
         ...state,
-        userRegisterError: null
-      }
+        userRegisterError: null,
+      };
     default:
       return state;
   }
-
-};
+}
