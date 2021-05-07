abdeb20a29485c1a3ab5ede016a189820d8410a3
Vaniko Akopashvili <49734341+VanoAkofashvili@users.noreply.github.com>
2021-05-07 14:03:24 +0400

    resolved conflicts

--- src/hooks/useUserRole.js
+++ src/hooks/useUserRole.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
-import { logOut } from "../redux/actions/authActions";
-=======
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
->>>>>>> 900954ccfc7fda713d8b238bbb7257e15970a4b4
+import { useSelector } from "react-redux";
 
 /* 
 Our route system depends on the role of the user
@@ -19,14 +12,14 @@ that's why I created this hook. user initialy gets role from local storage, but 
 is fires, userDetails (in redux user from db) variable is updates, and user gets role from redux
 */
 const useUserRole = () => {
-    const userDetails = useSelector(state => state.authReducer.userDetails)
-    const [role, setRole] = useState(localStorage.getItem('role'))
-    
-    useEffect(() => {
-        if (userDetails) {
-                setRole(userDetails.role)
-        }
-    }, [userDetails])
+  const userDetails = useSelector((state) => state.authReducer.userDetails);
+  const [role, setRole] = useState(localStorage.getItem("role"));
+
+  useEffect(() => {
+    if (userDetails) {
+      setRole(userDetails.role);
+    }
+  }, [userDetails]);
 
   return role;
 };
